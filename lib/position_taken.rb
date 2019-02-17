b475590ad5f307c2d3c1c12bb99ffb7eb313d8c5
Kesha C Lake <keshalakemusic@gmail.com>
2019-02-17 17:50:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ puts board.inspect
 puts index
 if board[index] == " " || board[index] == "" || board[index] = "nil" 
   return false
-else board[index] == "X" || board[index] == "O"
+else 
   return true
 end
 end 
